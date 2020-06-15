17243b8c96990b07afe5febf0d77ca92b0240054
Saqy <saqi@Adeels-MacBook-Pro.local>
2020-06-15 15:19:38 -0400

    feat(Prop-Types): adds prop types and default props

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import EmojiConvertor from 'emoji-js';
+import PropTypes from "prop-types";
+
 const Emoji = (props) => {
 var emoji = new EmojiConvertor();
   const {name, size} = props;
@@ -11,4 +13,14 @@ var emoji = new EmojiConvertor();
         </span>  
     );
 }
+
+Emoji.propTypes = {
+    name: PropTypes.string,
+    size: PropTypes.string,
+  };
+
+Emoji.defaultProps = {
+    name: "smile",
+    size: "20"
+  };
 export default Emoji;
